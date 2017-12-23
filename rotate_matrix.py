0d69520e6ab2331447fc7d32fa9e18aff4711667
Becca Rosenthal <beccarosenthal@gmail.com>
2017-12-23 15:38:39 -0800

    tests pass

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -1,24 +1,14 @@
 def rotate_matrix(list_of_lists):
     """
+    >>> lst_1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-    >>> rotate_matrix([[1, 2, 3], [4, 5, 6],
-            [7, 8, 9]])
-    [[7, 4, 1],
-    [8, 5, 2],
-    [9, 6, 3]]
+    >>> rotate_matrix(lst_1)
+    [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
 
-    >>> rotate_matrix(
-        [
-            [1, 2, 3, 4],
-            [5, 6, 7, 8],
-            [9, 10, 11, 12],
-            [13, 14, 15, 16]
-            ]
-        )
-    [[13, 9, 5, 1],
-    [14, 10, 6, 2],
-    [15, 11, 7, 3],
-    [16, 12, 8, 4]]
+    >>> lst_2 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+
+    >>> rotate_matrix(lst_2)
+    [[13, 9, 5, 1], [14, 10, 6, 2], [15, 11, 7, 3], [16, 12, 8, 4]]
 
     """
 
@@ -26,14 +16,14 @@ def rotate_matrix(list_of_lists):
 
     # Populate return list with correct number of lists
     for i in range(len(list_of_lists)):
-        print i
+        # print i
         row_to_add = []
         for j in range(-1, -1 *(len(list_of_lists) + 1), -1):
             row_to_add.append(list_of_lists[j][i])
-            print row_to_add
+            # print row_to_add
 
         return_list.append(row_to_add)
-        print return_list
+        # print return_list
 
 
     return return_list
